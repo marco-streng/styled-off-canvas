d6cca2c320efa08606f753db805a8db0f8953102
Marco Streng <marcostreng@me.com>
2019-11-28 11:55:32 +0100

    Adapted property order

--- src/Provider.js
+++ src/Provider.js
@@ -6,11 +6,11 @@ import Context from './Context'
 /**
  * Provider component
  *
+ * @param   {boolean}   closeOnEsc        if the menu should close on esc keydown
  * @param   {boolean}   isOpen            if the menu should be visible or not
  * @param   {string}    menuBackground    background color of the menu
  * @param   {string}    menuDuration      duration of the open/close animation for the menu
  * @param   {function}  onClose           callback function if menu closes
- * @param   {boolean}   closeOnEsc        if the menu should close on esc keydown
  * @param   {string}    overlayBackground background color of the menu
  * @param   {string}    overlayDuration   duration of the open/close animation for the overlay
  * @param   {string}    overlayOpacity    opacity of the overlay
@@ -20,11 +20,11 @@ import Context from './Context'
  */
 const Provider = ({
   children,
+  closeOnEsc = true,
   isOpen = false,
   menuBackground = '#fff',
   menuDuration = '500ms',
   onClose,
-  closeOnEsc = true,
   overlayBackground = '#000',
   overlayDuration = '500ms',
   overlayOpacity = 0.2,
@@ -33,11 +33,11 @@ const Provider = ({
 }) => (
   <Context.Provider
     value={{
-      isOpen,
-      menuDuration,
-      menuBackground,
-      onClose,
       closeOnEsc,
+      isOpen,
+      menuBackground,
+      menuDuration,
+      onClose,
       overlayBackground,
       overlayDuration,
       overlayOpacity,
