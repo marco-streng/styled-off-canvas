65ff65d81a1dae324cb9033d108775c31eef649a
Marco Streng <mstreng@bhs-corrugated.de>
2020-03-05 10:30:23 +0100

    Update styled-icons imports

--- example/components/Close.js
+++ example/components/Close.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Close as CloseIcon } from 'styled-icons/remix-fill/Close'
+import { Close as CloseIcon } from '@styled-icons/remix-fill/Close'
 
 export default ({ onClose }) => (
   <div
